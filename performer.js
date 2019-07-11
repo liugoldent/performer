80d5f0e82fcc5a9ab46c8063b5036ed0db62e9b2
Guan-Ting Liu <50863383+liugoldent@users.noreply.github.com>
2019-07-11 21:40:02 +0800

    Update performer.js

--- performer.js
+++ performer.js
@@ -10,7 +10,7 @@ if (w < 812 || w==980) {
   // var h = $('.nav').height();
   // var imgH = h + 50;
    $('.row>.FirstImg').css('top','5px');
-   $('#section5_icon').css('padding-right','3px');
+   $('#section5_icon').css('padding-right','10px');
 
 };
 
