967a92dc315476b903a262963683dc6c5d8467ae
GT Liu <liugoldent@gmail.com>
2019-07-11 20:38:55 +0800

    renew js title

--- performer.js
+++ performer.js
@@ -1,8 +1,21 @@
 var w = $(window).width();
+console.log(w);
 var img = w-100;
-if(w<1200){
-  $('img').css('width', img)
-}
-var h = $('.nav').height();
-var imgH = h+30;
-$('.row>.FirstImg').css('top',imgH);
+
+if (w < 812) {
+  $('ul.nav').css('display', 'block');
+  $('ul.nav').css('margin', 'auto');
+  var h = $('.nav').height();
+  var imgH = h + 50;
+  $('.row>.FirstImg').css('top', imgH);
+};
+
+
+// if(w<1200){
+//   $('.col-12>img').css('width', img);
+//   $('.col-xs-12>img').css('width', img);
+//   $('.col-md-12>img').css('width', img);
+// }
+
+
+
